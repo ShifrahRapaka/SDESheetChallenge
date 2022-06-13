46db27200c19edd514a0b3f05be8155c02c69fd5
ShifrahRapaka <57293466+ShifrahRapaka@users.noreply.github.com>
2022-06-13 22:43:32 +0530

    Add files via upload

---
+++ PalindromicLL.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+LinkedListNode<int>* reverseList(LinkedListNode<int>* head)
+    {
+        LinkedListNode<int>* pre=NULL;
+        LinkedListNode<int>* next=NULL;
+        while(head!=NULL)
+        {
+           next=head->next;
+            head->next=pre;
+            pre=head;
+            head=next;
+        }
+        return pre;
+    }
+bool isPalindrome(LinkedListNode<int> *head) {
+    // Write your code here.
+    if(head==NULL || head->next==NULL) return true;
+        LinkedListNode<int>* fast=head;
+        LinkedListNode<int>* slow=head;
+        
+        while(fast->next!=NULL &&fast->next->next!=NULL)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        slow->next=reverseList(slow->next);
+        slow=slow->next;
+        while(slow!=NULL)
+        {
+            if(head->data==slow->data)
+            {
+                head=head->next;
+                slow=slow->next;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        return true;
+
+}
